b709d6873c32ebadeb30829dec015d84c946a4ff
kamendkoleff <104923124+kamendkoleff@users.noreply.github.com>
2022-05-08 17:16:18 +0100

    Add files via upload

---
+++ Summator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+    namespace Summator
+    {
+          public class Program
+           {
+              static void Main(string[] args)
+               {
+            long result = Summator.Sum(new int[] { 1, 2, 3, 4, 5, 6, });
+            if (result == 21)
+            {
+                Console.WriteLine("TEST PASSED");
+            }
+            else
+            {
+                Console.WriteLine("TEST FAILED");
+
+            }
+            
+                long average = Summator.Average(new int[] { 5, 10, 15 });
+                if (average == 10)
+                {
+                    Console.WriteLine("TEST PASSED");
+                }
+                else
+                {
+                    Console.WriteLine("TEST FAILED");
+
+                }
+
+            }
+    }
+}
+
+
+
